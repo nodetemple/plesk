e56f5647ae19e9238d51a4ae7982b68ac0203abc
mnemotiv <hostmaster@nodetemple.com>
2016-04-07 12:11:15 +0300

    Workaround for mysql fix

--- 17.0/run.sh
+++ 17.0/run.sh
@@ -3,7 +3,7 @@
 echo "External IP: ${EXTERNAL_IP}"
 
 export PATH=/usr/lib/plesk-9.0:/usr/lib64/plesk-9.0:${PATH}
-#service mysql start
+service mysql start
 psa_service execute_actions
 psa_service startall
 plesk sbin pleskrc nginx start
